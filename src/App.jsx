9f4d51f71aa8998499dfb66072826900273979f7
duridrui <kanf2430@gmail.com>
2025-05-26 01:32:34 +0900

    feat: 오픈웨더 날씨 위젯을 할일 목록 안으로 위치 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
 import TodoList from './components/TodoList';
-import WeatherWidget from './components/WeatherWidget';
 
 function App() {
   return (
     <div className="App">
-      <WeatherWidget />
       <TodoList />
     </div>
   );
